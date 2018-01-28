67674b387c90baed2b9619ccea4ca99c9c568cba
Antoine Colmard <a.colmard@gmail.com>
2018-01-28 13:56:49 +0100

    :lipstick: Rework header colors

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,10 +6,7 @@ import style from './style.css';
 export default function Header() {
 	return (
 		<header class={style.header}>
-			<h1>Preact App</h1>
-			<nav>
-				<Link activeClassName={style.active} href="/">Home</Link>
-			</nav>
+			<h1><Link href="/">petite &middot; auth</Link></h1>
 		</header>
 	);
 }
