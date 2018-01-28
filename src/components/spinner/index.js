abf72502b2487b7340d8fa25d5bd188bfa9fc09e
Antoine Colmard <a.colmard@gmail.com>
2018-01-28 13:55:19 +0100

    :hammer: Allow `class` on Spinner

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,10 +1,11 @@
 import { h } from 'preact';
+import classNames from 'classnames';
 
 import style from './style.css';
 
-export default function Spinner() {
+export default function Spinner({ class: className, ...props }) {
 	return (
-		<div class={style.spinner}>
+		<div class={classNames(style.spinner, className)} {...props}>
 			<div class={style.spinner__circle} />
 		</div>
 	);
