73e3ba7e34c9f5c4b2e158c33c269e0f007751d0
Antoine Colmard <a.colmard@gmail.com>
2018-01-28 15:26:42 +0100

    :wrench: Automatically switch callback URL for production

--- preact.config.js
+++ preact.config.js
@@ -9,6 +9,8 @@ import webpack from 'webpack';
  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
  **/
 export default function (config, env, helpers) {
+	const isProduction = process.env.NODE_ENV === 'production';
+	const callbackUrl = isProduction ? 'https://petite-auth.surge.sh/callback' : 'http://localhost:8080/callback';
 	config.plugins = [
 		...config.plugins,
 		// Fixes `formidable` issue with ssr (https://github.com/felixge/node-formidable/issues/337#issuecomment-153408479)
@@ -20,7 +22,7 @@ export default function (config, env, helpers) {
 					AUTH0_CLIENT_ID: JSON.stringify('nCeCLL3DQ6IjDDovV30cbgyyWPd3akZ4'),
 					GOOGLE_CLIENT_ID: JSON.stringify('934399699312-niohk8v1b3bktl58n2ovuf9kmnbmkdai.apps.googleusercontent.com'),
 					FACEBOOK_CLIENT_ID: JSON.stringify('2023835517832584'),
-					CALLBACK_URL: JSON.stringify('https://petite-auth.surge.sh/callback')
+					CALLBACK_URL: JSON.stringify(callbackUrl)
 				}
 			}
 		})
