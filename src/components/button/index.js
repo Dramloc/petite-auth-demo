24328677fbc3842951fdce7c2545a53332017ad4
Antoine Colmard <a.colmard@gmail.com>
2018-01-30 15:59:59 +0100

    :sparkles: Add getting started button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -4,6 +4,13 @@ import classNames from 'classnames';
 import style from './style.css';
 
 export default function Button({ class: className, children, ...props }) {
+	if (props.href) {
+		return (
+			<a class={classNames(style.button, className)} {...props}>
+				<div class={style.button__overlay}>{children}</div>
+			</a>
+		);
+	}
 	return (
 		<button class={classNames(style.button, className)} {...props}>
 			<div class={style.button__overlay}>{children}</div>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -20,7 +20,9 @@ export default function Home() {
 				<h1>Petite · Auth</h1>
 				<Logo class={style.home__logo} />
 			</div>
-			<p>🔒 Tiny (520B) JavaScript OAuth2 client</p>
+			<p>🔒 Tiny (429B) JavaScript OAuth2 client</p>
+
+			<Button class={style['button--getting-started']} href="https://github.com/Dramloc/petite-auth">Getting started</Button>
 
 			<h2>Try it!</h2>
 			<h3>Auth0</h3>
